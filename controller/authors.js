8613351af49e0ddc8a79a94055e95d3fd75b106e
8ahmadreza3 <8arhp3@gmail.com>
2024-03-20 15:23:45 +0330

    authors pair buttons

--- controller/authors.js
+++ controller/authors.js
@@ -4,16 +4,25 @@ const component = require('../component')
 const axios = require('axios')
 
 const createPairbutton = (authors) => {
-  const buttons = authors.map((author, index) => {
-    const nextAuthor = authors[index + 1]
-    return [
-      { text: author.name, url: website + 'author/' + author.address },
-      nextAuthor ? { text: nextAuthor.name, url: website + 'author/' + nextAuthor.address } : null
-    ].filter(Boolean)
-  })
-  const { length } = authors
-  if (length % 2 === 0) {
-    buttons[length - 1].push({ text: authors[length - 1].name, url: website + 'author/' + authors[length - 1].address })
+  const buttons = []
+  for (let i = 0; i + 1 < authors.length; i += 2) {
+    const pair = [authors[i]]
+    if (i + 1 < authors.length) {
+      pair.push(authors[i + 1])
+    }
+    buttons.push(
+      pair.map((auhtor) => ({
+        text: auhtor.name,
+        url: website + 'author/' + auhtor.address
+      }))
+    )
+  }
+  if (authors.length % 2 !== 0) {
+    const lastAuhtors = [{
+      text: authors[authors.length - 1].name,
+      url: website + 'author/' + authors[authors.length - 1].address
+    }]
+    buttons.push(lastAuhtors)
   }
   return buttons
 }
@@ -29,8 +38,8 @@ module.exports = async () => {
     })
   const inlineKeyboardInfo = [
     ...createPairbutton(authors),
-    [{ text: 'نویسندگان بیشتر', url: website + 'authors' }],
-    [component.backButoon()]
+    [{ text: 'نویسندگان بیشتر✍🏼', url: website + 'authors' },
+      component.backButoon()]
   ]
   const authorsOptions = {
     reply_markup: {
