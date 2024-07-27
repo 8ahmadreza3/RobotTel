766a7424d6d9a5e907420778ef5e15b40d72bd05
8ahmadreza3 <8arhp3@gmail.com>
2024-07-27 11:15:02 +0330

    refactoring

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
-const { token, port } = require('./config')
+const { token } = require('./config')
 const bot = new TelegramBot(token, { polling: true })
 const bootApp = require('./app')
-const express = require('express')
-const app = express()
 
-app.listen(port, () => {
-  console.log(`app is running on port:${port}`)
-})
-app.get('/', (req, res) => {
-  res.send('app is running')
-})
 bootApp(bot)
